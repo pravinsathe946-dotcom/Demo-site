d01e2b104cd8305001414d22027cb55c78ac7c94
pravinsathe946-dotcom <pravinsathe946@gmail.com>
2026-06-18 19:05:16 +0530

    added custom page template

---
+++ wp-content/themes/twentytwentyfour/templates/page-custom.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template Name: My Custom Layout
+*/
+get_header(); // Pulls in your site header
+?>
+
+<div class="custom-content">
+    <!-- Add your HTML or custom PHP loop content here -->
+    <?php while ( have_posts() ) : the_post(); the_content(); endwhile; ?>
+    <h2>Hi, this is custopm page template </h2>
+</div>
+
+<?php get_footer(); // Pulls in your site footer ?>
