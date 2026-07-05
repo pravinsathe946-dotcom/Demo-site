08b41bb94feeaa7e142c162e9920510288ec46fd
pravinsathe946-dotcom <pravinsathe946@gmail.com>
2026-07-05 22:21:01 +0530

    Add AWS settings to wp-config.php
    Added AS3CF_SETTINGS for AWS configuration.

--- wp-config.php
+++ wp-config.php
@@ -36,7 +36,11 @@ define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
-
+define( 'AS3CF_SETTINGS', serialize( array(
+    'provider' => 'aws',
+    'access-key-id' => 'AKIA4YQACMNWRMHE2IQ7',
+    'secret-access-key' => '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',
+) ) );
 /**#@+
  * Authentication unique keys and salts.
  *
